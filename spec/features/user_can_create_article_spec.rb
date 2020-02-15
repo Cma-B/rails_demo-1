48080b7a9f76780b4d52c59687acce3ec1fea08a
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-15 12:43:19 +0100

    starting to write the test to successfully create an article

--- spec/features/user_can_create_article_spec.rb
+++ spec/features/user_can_create_article_spec.rb
@@ -1,3 +1,19 @@
 feature 'User can create articles' do
-    
+    before do 
+        visit root_path
+        click_on "New Article"
+    end
+
+    context 'Successfully create an article' do 
+        before do 
+            fill_in 'Title', with: 'Happy holidays'
+            fill_in 'Content', with: 'Buy your gifts now'
+            click_on 'Create article'
+        end
+
+        it 'User should see article on show page' do
+            article = Article.find_by(title: 'Happy holidays')
+            expect(current_path).to eq article_path(article)
+        end
+    end
 end
