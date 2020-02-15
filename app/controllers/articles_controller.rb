c1d02fec6b9e05fd70f7ef6775617932a28af12c
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-15 20:25:41 +0100

    just realised that i named this branch completely wrong due to the last project, sloppy of me. Struggling on error that the link to new article cant be found# #

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(params[:article])
+  def new
+    @article = Article.new
+  end
 
-    @article.save
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- spec/features/user_can_create_article_spec.rb
+++ spec/features/user_can_create_article_spec.rb
@@ -1,19 +1,20 @@
 feature 'User can create articles' do
     before do 
-        visit root_path
-        click_on "New Article"
+        visit articles_index_path
     end
 
     context 'Successfully create an article' do 
         before do 
+            click_on 'New Article'
             fill_in 'Title', with: 'Happy holidays'
             fill_in 'Content', with: 'Buy your gifts now'
             click_on 'Create article'
         end
-
-        it 'User should see article on show page' do
+    
+        it 'User should bee article on show page' do
+            visit articles_path
             article = Article.find_by(title: 'Happy holidays')
             expect(current_path).to eq article_path(article)
-        end
+        end 
     end
 end
